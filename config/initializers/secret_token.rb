4be9f48ebf955e14b270c11676133ad19aa5a67d
Beat Besmer <beat.besmer@gmail.com>
2013-02-24 17:03:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FairyTiles::Application.config.secret_token = '3a1bbf89ad94f2106a8d6b3973f58040dec16f6a6e03cdfdd526fa005c3593f665c1e624fba72222f480c029f7490b40f7a471eef68c4401b200bc0cd1787ee5'
